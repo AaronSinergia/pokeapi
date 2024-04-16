02266f4d56c824d4b18d697e2dc818a9755e2069
Aaron <aaronromeromusic@gmail.com>
2024-04-16 20:42:41 +0200

    proyecto finish

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ En esta Pokedex puedes obtener info de 3 formas:
 
 2 - Puedes poner en tu navegador al final de la URL ej. /pikachu y te pinta el pikachu en pantalla.
 
-3 - Puedes pulsar el botón de "PokeSearch", y buscar en el input a tu Pokemon favorito.
+3 - Puedes pulsar el botón de "PokeSearch", y buscar en el input a tu Pokemon favorito también.
--- src/pages/components/PokemonChoosed/PokemonChoosed.jsx
+++ src/pages/components/PokemonChoosed/PokemonChoosed.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './PokemonChoosed.css';
-import PokedexBckgrnd from '../PokedexBckgrnd/PokedexBckgrnd';
 import Footer from '../Footer/Footer';
 
 const PokemonChoosed = ({ pokemon }) => {
   return (
     <>
-      <PokedexBckgrnd />
-      <h3 className="pokemon_titlename">{pokemon.name}</h3>
-      <h3 className="pokemon_type"> TYPE: {pokemon.types[0].type.name}</h3>
-      <div className="div_pkm_img">
-        <img
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
-          className="pokemon_img"
-        />
+      <div className="pkmn_choosed">
+        <img src="../../../../public/PngItem_2227129.png" alt="pokedex_photo" />
+        <h3 className="pokemon_titlename">{pokemon.name}</h3>
+        <h3 className="pokemon_type"> TYPE: {pokemon.types[0].type.name}</h3>
+        <div className="div_pkm_img">
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            className="pokemon_img"
+          />
+        </div>
       </div>
       <Footer />
     </>
